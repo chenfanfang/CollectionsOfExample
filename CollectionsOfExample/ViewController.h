bee238cbb45ad3f120ab6152363ece8f392afc0d
陈蕃坊 <chenfanfang@dandanlicai.com>
2016-07-17 17:48:28 +0800

    Initial Commit

---
+++ CollectionsOfExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionsOfExample
+//
+//  Created by mac on 16/7/17.
+//  Copyright © 2016年 chenfanfang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CollectionsOfExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CollectionsOfExample
+//
+//  Created by mac on 16/7/17.
+//  Copyright © 2016年 chenfanfang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
